ea4d6569a1c61dd6abcaf364139765d2a6b80ee6
dobri3 <bortgr@gmail.com>
2026-04-02 14:04:36 +0300

    added api, domain, services

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Movie, Genre, Watchlist
+from ..domain.models import Movie, Genre, Watchlist
 
 
 class GenreSerializer(serializers.ModelSerializer):
