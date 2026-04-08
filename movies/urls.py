259da416d66de90f090e00df2171347e66148a75
lrmpsm <LevLisitzyn@yandex.com>
2026-04-08 15:59:46 +0300

    removed unnecessary urls.py in movies django-app

--- movies/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from api.views import MovieViewSet, WatchlistViewSet
-
-router = DefaultRouter()
-router.register(r'movies', MovieViewSet, basename='movies')
-router.register(r'watchlist', WatchlistViewSet, basename='watchlist')
-
-urlpatterns = router.urls
