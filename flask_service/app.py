5b44c238daf2be3c53f3b4f6ddf044cdb1a880b2
Roman Shashkov <em.krem08@gmail.com>
2026-05-23 16:48:26 +0300

    UGC model added (task 1.2)

--- flask_service/app.py
+++ flask_service/app.py
@@ -1,16 +1,36 @@
 import os
 from flask import Flask, jsonify
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 load_dotenv()
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///ugc.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+
+
+class UGC(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), nullable=False)
+    text = db.Column(db.Text, nullable=True)
+    rating = db.Column(db.Integer, nullable=True)
+    status = db.Column(db.String(50), default='pending')
+    movie_id = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/health', methods=['GET'])
 def health_check():
     return jsonify({"status": "ok"})
-
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8002))
