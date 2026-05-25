67b35e20354f37a67cb48312c829cd4e4a20b6e9
Roman Shashkov <em.krem08@gmail.com>
2026-05-25 13:04:29 +0300

    database created

--- fastapi_service/database.py
+++ fastapi_service/database.py
@@ -1,1 +1,18 @@
-fake_users_db: dict = {}
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/cinema"
+
+engine = create_async_engine(DATABASE_URL)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+Base = declarative_base()
