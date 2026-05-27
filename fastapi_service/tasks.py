02b85a2bbad5eed5e4a2055bee7e46cdff0c2b8b
Roman Shashkov <em.krem08@gmail.com>
2026-05-27 15:01:16 +0300

    logging on error added

--- fastapi_service/tasks.py
+++ fastapi_service/tasks.py
@@ -2,6 +2,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def notify_user(user_email: str, movie_id: int):
-    logger.info(f"[background] Начало уведомления: {user_email}, фильм {movie_id}")
-    logger.info(f"[background] Уведомление отправлено: {user_email}")
+def notify_user(user_id: int, movie_id: int):
+    try:
+        logger.info(f"[background] Начало уведомления: user={user_id}, movie={movie_id}")
+        logger.info(f"[background] Уведомление отправлено: user={user_id}, movie={movie_id}")
+    except Exception as e:
+        logger.error(f"Ошибка сервиса уведомлений: {e}")
